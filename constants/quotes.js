ec3bbac8221d068938e27a787be9415600c09df1
Dylandk10 <Dylandk10@gmail.com>
2022-10-27 14:29:33 -0400

    added more quotes

--- constants/quotes.js
+++ constants/quotes.js
@@ -7,7 +7,7 @@ export const QUOTES = [
         option3: ""
     },
     {
-        quote: "You are so brave and quit I forget you are suffering",
+        quote: "You are so brave and quit I forget you are suffering.",
         author: "Ernest Hemingway",
         option1: "",
         option2: "",
@@ -42,7 +42,7 @@ export const QUOTES = [
         option3: ""
     },
     {
-        quote: "There is hope, even when your brain tells you there isn't",
+        quote: "There is hope, even when your brain tells you there isn't.",
         author: "John Green",
         option1: "",
         option2: "",
@@ -77,7 +77,7 @@ export const QUOTES = [
         option3: ""
     },
     {
-        quote: "Change your life today. Don't gamble on the future, act now, without delay",
+        quote: "Change your life today. Don't gamble on the future, act now, without delay.",
         author: "Simone de Beauvoir",
         option1: "",
         option2: "",
@@ -107,6 +107,244 @@ export const QUOTES = [
     {
         quote: "No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.",
         author: "Buddha",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "When you have a dream, you've got to grab it and and never let go.",
+        author: "Carol Burnett",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Nothing is impossible. The wird itsself says I'm possible!",
+        author: "Audrey Hepburn",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "The bad news is time flies. The good news is your the pilot.",
+        author: "Michael altshuler",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        author: "Winston Churchill",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Spread love everywhere you go.",
+        author: "Mother Teresa",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "You do not find the happy life. You make it.",
+        author: "Camilla Eyring Kimball",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Whatever you think the world is withholding from you, you are withholding from the world.",
+        author: "Eckhart Tolle",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Thinking: the talking of the soul itself.",
+        author: "Plato",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "From a small seed a mighty trunk may grow.",
+        author: "Aeschylus",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "I believe every human has finite number of hearbeats. I don't intend to waste any of mine.",
+        author: "Neil Armstrong",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Learning how too be still, ready to be still and let life happen - that stillness becomes radiance.",
+        author: "Morgan Freeman",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "If you accept the expectations of other, especially negative ones, then you never will change the outcome.",
+        author: "Michael Jordan",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Vitality shows in not only the ability to persist but the ability to start over.",
+        author: "F. Scott Fitzgerald",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Be faithful to that which exist within yourself.",
+        author: "Andre Gide",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Each day provides its own gifts.",
+        author: "Marcus Aurelius",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Health is the greatest possession. Contentment is the greatest treasure. Confidence is the greatest friend. Non-being is the greatest joy.",
+        author: "Lao Tzu",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "What we Think, we become.",
+        author: "Buddha",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "I celebrate myself, and sing myself.",
+        author: "Walt Whitman",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Look within. Within is the fountain of good, and it will ever bubble up, if thou wult ever dig.",
+        author: "Marcus Aurelius",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Space is a inspirational concept that allows you to dream big.",
+        author: "Peter Diamandis",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "The most inspirational person in my life has been my mother.",
+        author: "Nicole Maines",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "No man ever steps in the same river twice, for it's not the same river and he's not the same man.",
+        author: "Heraclitus",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.",
+        author: "Mark Twain",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "The ones who are crazy enough to think that they can change the world, are the ones who do.",
+        author: "Steve Jobs",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Life without experience and suffering is not life.",
+        author: "Socrates",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Life is too deep for words, so don't try to describe it, just live it.",
+        author: "C.S. Lewis",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "The more man meditates upon good thoughts, the better will be his world and the world at large.",
+        author: "Confucius",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Wherever you go, go with all your heart.",
+        author: "Confusius",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "The madness of love is the greatest of heaven's blessings.",
+        author: "Plato",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Mindfulness is above all the capacity simply to recognize the presence of an object without taking sides, without judging, and without craving or despising that object.",
+        author: "Thich Nhat Hanh",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "Don't hurry anything. Don't worry about the future. Don't worry about what progress your making. Just be entirely content to be aware of what is.",
+        author: "Alan Watts",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "An ounce of action is worth a ton of theory.",
+        author: "Ralph Waldo Emerson",
+        option1: "",
+        option2: "",
+        option3: ""
+    },
+    {
+        quote: "It's strange how simple things become, once you see them clearly.",
+        author: "Ayn Rand",
         option1: "",
         option2: "",
         option3: ""
